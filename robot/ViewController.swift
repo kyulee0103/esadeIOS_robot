8747d27a24e61729d468ac7147eea239fa632e9c
kyulee0103 <kyulee1338@gmail.com>
2023-11-14 15:33:12 +0100

    Initial Commit

---
+++ robot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  robot
+//
+//  Created by 권규리 on 2023/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
